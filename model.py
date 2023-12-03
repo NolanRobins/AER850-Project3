041dea836cf5751dddfb742d13ce41b6ebb37a92
NolanRobins <56159640+NolanRobins@users.noreply.github.com>
2023-12-03 17:49:22 -0500

    Setup model training and trained 200 epochs

--- model.py
+++ model.py
@@ -1,7 +1,15 @@
 from ultralytics import YOLO
 
+IMAGE_SIZE = 1216
 
-model = YOLO('yolov8n.pt')
+def train_model():
+    model = YOLO("best.pt")
+    results = model.train(data = "data.yaml", epochs = 100, imgsz = IMAGE_SIZE, batch = 2)
 
-results = model.train(data='data.yaml', epochs=100, imgsz=640, batch = 10)
+def predict_model():
+    model = YOLO("runs/detect/train8/weights/best.pt")
+    model.predict('datasets/test/images/ArduinoMega_Top_jpg.rf.4296818296b50ae297ac4efaa75b8df9.jpg', save=True, imgsz = IMAGE_SIZE, conf = 0.5)
 
+
+if __name__ == "__main__":
+    train_model()
