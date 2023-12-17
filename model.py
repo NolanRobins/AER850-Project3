c5b9c7ef8bcbf16a0c29f88d89fd11760c9e8996
NolanRobins <56159640+NolanRobins@users.noreply.github.com>
2023-12-17 03:03:22 -0500

    😭 Added ➕ medium 🥈👈 sized model 💪 with 500 epochs
    This took 8 hours

--- model.py
+++ model.py
@@ -1,16 +1,20 @@
 from ultralytics import YOLO
 import cv2
 
-
+OUTPUT_DIR = "outputs/predictions/"
 IMAGE_SIZE = 1216
 
 def train_model():
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8m.pt")
     results = model.train(data = "data.yaml", epochs = 600, imgsz = IMAGE_SIZE, batch = 2)
 
+def resume_training():
+    model = YOLO("runs/detect/train/weights/last.pt")
+    results = model.train(data = "data.yaml", epochs = 600, imgsz = IMAGE_SIZE, batch = 2, resume = True)
+
 def predict_model():
-    model = YOLO("best.pt")
-    results = model.predict('datasets/evaluation', save=False, imgsz = IMAGE_SIZE, conf = 0.5)
+    model = YOLO("runs/detect/train/weights/best.pt")
+    results = model.predict('datasets/evaluation', save=False, imgsz = IMAGE_SIZE, conf = 0.5, max_det = 2000)
 
     for pred in results:
         draw_prediction(pred.cpu())
@@ -18,20 +22,30 @@ def predict_model():
 def draw_prediction(prediction):
     image = cv2.imread(prediction.path)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 2
-    font_thickness = 2
-    color = (0, 255, 0) 
+    font_scale = 1 * prediction.orig_shape[0]/1300
+    font_thickness = int(prediction.orig_shape[0]/1000)
+    color = [(128, 0, 0), (154, 99, 36), (70, 153, 144), (0, 0, 117), (230, 25, 75), (245, 130, 49), (255, 225, 25), (60, 180, 75), (66, 212, 244), (67, 99, 216), (240, 50, 230)]
+
 
     for bbox, label, confidence in zip(prediction.boxes.xyxy.numpy(), prediction.boxes.cls.numpy(), prediction.boxes.conf.numpy()):
         bbox = [ int(x) for x in bbox ]
         left, top, right, bottom = bbox
-        cv2.rectangle(image, (left, top), (right, bottom), color, font_thickness)
+        cv2.rectangle(image, (left, top), (right, bottom), color[int(label)], font_thickness)
+        
+
+    for bbox, label, confidence in zip(prediction.boxes.xyxy.numpy(), prediction.boxes.cls.numpy(), prediction.boxes.conf.numpy()):
+        bbox = [ int(x) for x in bbox ]
+        left, top, right, bottom = bbox
         text = f"{prediction.names[label]}: {confidence:.2f}"
-        cv2.putText(image, text, (left, top - 10), font, font_scale, color, font_thickness)
+        text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+        cv2.rectangle(image, (left, top - 10 - text_size[1]), (left + text_size[0], top - 10), color[int(label)], -1)
+        cv2.putText(image, text, (left, top - 10), font, font_scale, (255, 255, 255), font_thickness)
+    
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    cv2.imwrite(OUTPUT_DIR + prediction.path.split("/")[-1], image)
 
-    cv2.imshow('Predictions', image)
+    # cv2.imshow('Predictions', image)
 
 if __name__ == "__main__":
     predict_model()
